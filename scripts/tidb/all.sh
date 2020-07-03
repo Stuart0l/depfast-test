920133d4783f06d15748fbde2fc2141c67c40036
pentium3 <celerond@msn.com>
2020-07-03 11:34:19 +0000

    result for tidb

--- scripts/tidb/all.sh
+++ scripts/tidb/all.sh
@@ -1,11 +1,13 @@
-./start_experiment.sh 5 ./workloada 600 1 azure leader
-./start_experiment.sh 5 ./workloada 600 2 azure leader
-./start_experiment.sh 5 ./workloada 600 5 azure leader
+#./start_experiment.sh 5 ./workloada 600 2 azure leader swapoff mem
+#./start_experiment.sh 5 ./workloada 600 5 azure leader swapoff mem
 
-./start_experiment.sh 5 ./workloada 600 1 azure noslow1
-./start_experiment.sh 5 ./workloada 600 1 azure noslow2
+#./start_experiment.sh 10 ./workloada 600 1 azure noslow1 swapoff mem
+#./start_experiment.sh 10 ./workloada 600 1 azure noslow2 swapoff mem
 
-./start_experiment.sh 5 ./workloada 600 1 azure follower
-./start_experiment.sh 5 ./workloada 600 2 azure follower
-./start_experiment.sh 5 ./workloada 600 5 azure follower
+#./start_experiment.sh 5 ./workloada 600 1 azure follower swapoff mem
+#./start_experiment.sh 5 ./workloada 600 2 azure follower swapoff mem
+#./start_experiment.sh 5 ./workloada 600 5 azure follower swapoff mem
+./start_experiment.sh 1 ./workloada 600 1 azure leader swapoff mem
+
+
 
