468c1d213e474be056b23906068221ee1db8cd91
pentium3 <celerond@msn.com>
2020-07-04 17:34:35 +0000

    ..

---
+++ scripts/tidb/all_hdd.sh
@@ -0,0 +1,17 @@
+./start_experiment.sh 10 ./workloada 600 1 azure leaderhigh swapoff hdd
+./start_experiment.sh 10 ./workloada 600 2 azure leaderhigh swapoff hdd
+./start_experiment.sh 10 ./workloada 600 3 azure leaderhigh swapoff hdd
+./start_experiment.sh 10 ./workloada 600 4 azure leaderhigh swapoff hdd
+./start_experiment.sh 10 ./workloada 600 5 azure leaderhigh swapoff hdd
+
+#./start_experiment.sh 10 ./workloada 600 1 azure noslow1 swapoff hdd
+#./start_experiment.sh 10 ./workloada 600 1 azure noslow2 swapoff hdd
+
+#./start_experiment.sh 10 ./workloada 600 1 azure follower swapoff hdd
+#./start_experiment.sh 10 ./workloada 600 2 azure follower swapoff hdd
+#./start_experiment.sh 10 ./workloada 600 3 azure follower swapoff hdd
+#./start_experiment.sh 10 ./workloada 600 4 azure follower swapoff hdd
+#./start_experiment.sh 10 ./workloada 600 5 azure follower swapoff hdd
+
+
+
