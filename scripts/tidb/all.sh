76d2658d782040f34733e5be535cb6414db0e921
pentium3 <celerond@msn.com>
2020-07-09 09:18:11 +0000

    tidb

--- scripts/tidb/all.sh
+++ scripts/tidb/all.sh
@@ -7,7 +7,10 @@
 #./start_experiment.sh 5 ./workloada 600 1 azure follower swapoff mem
 #./start_experiment.sh 5 ./workloada 600 2 azure follower swapoff mem
 #./start_experiment.sh 5 ./workloada 600 5 azure follower swapoff mem
-./start_experiment.sh 1 ./workloada 600 1 azure leader swapoff mem
+#./start_experiment.sh 1 ./workloada 600 1 azure leader swapoff mem
 
-
+#./start_experiment.sh 8 ./workloada 600 6 azure leaderhigh swapon mem
+#./start_experiment.sh 10 ./workloada 600 6 azure follower swapon mem
+./start_experiment.sh 5 ./workloada 600 6 azure noslow1 swapon mem
+./start_experiment.sh 5 ./workloada 600 6 azure noslow2 swapon mem
 
