c35bd542bbe991d42509bf0335be102a7ae9f52b
pentium3 <celerond@msn.com>
2020-07-14 20:38:51 -0500

    ...

--- scripts/mongodb/start_experiment.sh
+++ scripts/mongodb/start_experiment.sh
@@ -18,7 +18,7 @@ s3name="mongodb3"
 serverZone="us-central1-a"
 ###########################
 
-if [ "$#" -ne 6 ]; then
+if [ "$#" -ne 8 ]; then
     echo "Wrong number of parameters"
     echo "1st arg - number of iterations"
     echo "2nd arg - workload path"
