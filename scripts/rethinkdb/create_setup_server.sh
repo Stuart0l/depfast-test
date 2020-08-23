19627f5aa7346687651d66c1c6abb5956c5e91dc
ritesh <ritesh_sinha@ymail.com>
2020-08-23 02:19:50 -0700

    Minor fix for subscription change

--- scripts/rethinkdb/create_setup_server.sh
+++ scripts/rethinkdb/create_setup_server.sh
@@ -29,13 +29,12 @@ threadsycsb=$7
 username="riteshsinha"
 resource="DepFast"
 serverRegex="rethinkdb$namePrefix-[1-$noOfServers]"
-subscriptionName="Azure for Students"
 declare -A serverNameIPMap
 
 # Create the VM on Azure
 function az_vm_create {
   # Create client VM
-  az vm create --name rethinkdb"$namePrefix"-client --resource-group DepFast --subscription '$subscriptionName' --zone 1 --image debian --os-disk-size-gb 128 --storage-sku Standard_LRS --size Standard_D4s_v3 --admin-username riteshsinha --ssh-key-values ~/.ssh/id_rsa.pub --accelerated-networking true
+  az vm create --name rethinkdb"$namePrefix"-client --resource-group DepFast --subscription 'Azure for Students' --zone 1 --image debian --os-disk-size-gb 128 --storage-sku Standard_LRS --size Standard_D4s_v3 --admin-username riteshsinha --ssh-key-values ~/.ssh/id_rsa.pub --accelerated-networking true
 
   # Setup Client IP and name
   clientConfig=$(az vm list-ip-addresses --name rethinkdb"$namePrefix"-client --query '[0].{name:virtualMachine.name, privateip:virtualMachine.network.privateIpAddresses[0], publicip:virtualMachine.network.publicIpAddresses[0].ipAddress}' -o json)
@@ -57,7 +56,7 @@ function az_vm_create {
   # Create servers with both local ssh key and client VM ssh key
   for (( i=1; i<=noOfServers; i++ ))
   do
-    az vm create --name rethinkdb"$namePrefix"-"$i" --resource-group DepFast --subscription '$subscriptionName' --zone 1 --image debian --os-disk-size-gb 64 --storage-sku Standard_LRS --data-disk-sizes-gb 128 --size Standard_D4s_v3 --admin-username riteshsinha --ssh-key-values ~/.ssh/id_rsa.pub ./client_rsa.pub --accelerated-networking true
+    az vm create --name rethinkdb"$namePrefix"-"$i" --resource-group DepFast --subscription 'Azure for Students' --zone 1 --image debian --os-disk-size-gb 64 --storage-sku Standard_LRS --data-disk-sizes-gb 128 --size Standard_D4s_v3 --admin-username riteshsinha --ssh-key-values ~/.ssh/id_rsa.pub ./client_rsa.pub --accelerated-networking true
   done
 }
 
@@ -108,18 +107,18 @@ function run_ssd_experiment {
 }
 
 function run_memory_experiment {
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure noslow memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure noslow memory swapoff 3 $serverRegex $threadsycsb)"
 
+
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 2 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 5 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 6 azure leader memory swapon 3 $serverRegex $threadsycsb)"
+
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 2 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
+	#ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 5 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
 	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure noslow memory swapon 3 $serverRegex $threadsycsb)"
-
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 2 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 5 azure leader memory swapoff 3 $serverRegex $threadsycsb)"
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 6 azure leader memory swapon 3 $serverRegex $threadsycsb)"
-
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 2 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
-	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 5 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
 	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $clientPublicIP "(cd ~/YCSB/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 6 azure follower memory swapon 3 $serverRegex $threadsycsb)"
 }
 
@@ -162,10 +161,12 @@ function deallocate_vms {
 		# Switching back to linear stop
 		for key in "${!serverNameIPMap[@]}";
 		do
+			az vm stop --name "$key" --resource-group "$resource"
 			az vm deallocate --name "$key" --resource-group "$resource"
 		done
 	fi
 	# Deallocate client VM as well
+	az vm stop --name "$clientName" --resource-group "$resource"
 	az vm deallocate --name "$clientName" --resource-group "$resource"
 }
 
