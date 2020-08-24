cbd63b15b0bd3293dbfeb29016ece96bd5a62f7b
pentium3 <celerond@msn.com>
2020-08-24 00:54:36 +0000

    ..

--- scripts/mongodb/all_mem.sh
+++ scripts/mongodb/all_mem.sh
@@ -1,12 +1,12 @@
-./start_experiment.sh 5 workloada 300 1 azure leader swapoff mem
-./start_experiment.sh 5 workloada 300 2 azure leader swapoff mem
-./start_experiment.sh 5 workloada 300 5 azure leader swapoff mem
-./start_experiment.sh 5 workloada 300 6 azure leader swapon mem
+#./start_experiment.sh 5 workloada 300 1 azure leader swapoff mem
+#./start_experiment.sh 5 workloada 300 2 azure leader swapoff mem
+#./start_experiment.sh 5 workloada 300 5 azure leader swapoff mem
+#./start_experiment.sh 5 workloada 300 6 azure leader swapon mem
 
-./start_experiment.sh 5 workloada 300 1 azure noslow swapoff mem
-./start_experiment.sh 5 workloada 300 1 azure noslow swapon mem
+./start_experiment.sh 3 workloada 300 1 azure noslow swapoff mem
+./start_experiment.sh 3 workloada 300 1 azure noslow swapon mem
 
-./start_experiment.sh 5 workloada 300 1 azure follower swapoff mem
-./start_experiment.sh 5 workloada 300 2 azure follower swapoff mem
-./start_experiment.sh 5 workloada 300 5 azure follower swapoff mem
-./start_experiment.sh 5 workloada 300 6 azure follower swapon mem
+./start_experiment.sh 3 workloada 300 1 azure follower swapoff mem
+./start_experiment.sh 3 workloada 300 2 azure follower swapoff mem
+./start_experiment.sh 3 workloada 300 5 azure follower swapoff mem
+./start_experiment.sh 3 workloada 300 6 azure follower swapon mem
