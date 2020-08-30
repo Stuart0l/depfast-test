2c54f4dab1fe2da2da1bbaef5bc1d0725562fffb
pentium3 <celerond@msn.com>
2020-08-30 03:28:45 +0000

    ..

--- scripts/mongodb/all_saturate_mem.sh
+++ scripts/mongodb/all_saturate_mem.sh
@@ -1,10 +1,10 @@
 #./start_experiment.sh 5 workloads 300 1 azure leader swapoff mem
 #./start_experiment.sh 5 workloads 300 2 azure leader swapoff mem
 #./start_experiment.sh 5 workloads 300 5 azure leader swapoff mem
-./start_experiment.sh 5 workloads 300 6 azure leader swapon mem
+#./start_experiment.sh 5 workloads 300 6 azure leader swapon mem
 
 ./start_experiment.sh 5 workloads 300 1 azure noslow swapoff mem
-#./start_experiment.sh 5 workloads 300 1 azure noslow swapon mem
+./start_experiment.sh 5 workloads 300 1 azure noslow swapon mem
 
 #./start_experiment.sh 5 workloads 300 1 azure follower swapoff mem
 #./start_experiment.sh 5 workloads 300 2 azure follower swapoff mem
