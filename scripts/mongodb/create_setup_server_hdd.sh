4f6026c0625516226a84ff3076d115ee60aa2499
pentium3 <celerond@msn.com>
2020-09-07 01:07:56 -0500

    ...

---
+++ scripts/mongodb/create_setup_server_hdd.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+
+date=$(date +"%Y%m%d%s")
+# exec > "$date"_setupserver.log
+# exec 2>&1
+
+set -ex
+
+if [ "$#" -ne 7 ]; then
+  echo "Wrong number of args"
+  echo "1st arg - number of servers to create"
+  echo "2nd arg - server prefix name(-1,-2,-3 added as suffix to this name)"
+  echo "3rd arg - number of iterations"
+  echo "4th arg - workload name"
+  echo "5th arg - seconds to run ycsb run"
+  echo "6th arg - file system to use(disk,memory)"
+  echo "7th arg - ycsb run threads(for saturation exp)"
+  exit 1
+fi
+
+noOfServers=$1
+namePrefix=$2
+iterations=$3
+workload=$4
+ycsbruntime=$5
+filesystem=$6
+threadsycsb=$7
+
+resource="DepFast3"
+serverRegex="mongodb$namePrefix-[1-$noOfServers]"
+declare -A serverNameIPMap
+
+function setup_localvm {
+  clientPublicIP="192.168.1.19"
+  serverNameIPMap["s1"]="192.168.1.15"
+  serverNameIPMap["s2"]="192.168.1.16"
+  serverNameIPMap["s3"]="192.168.1.18"
+}
+
+# Create the VM on Azure
+function az_vm_create {
+  rm -f ~/.ssh/known_hosts
+
+  # Create client VM
+  az vm create --name mongodb"$namePrefix"-client --resource-group DepFast3 --subscription 'Last Chance' --zone 1 --image debian --os-disk-size-gb 64 --storage-sku Standard_LRS  --size Standard_D4s_v3 --admin-username tidb --ssh-key-values ~/.ssh/id_rsa.pub --accelerated-networking true
+  # Setup Client IP and name
+  clientConfig=$(az vm list-ip-addresses --name mongodb"$namePrefix"-client --query '[0].{name:virtualMachine.name, privateip:virtualMachine.network.privateIpAddresses[0], publicip:virtualMachine.network.publicIpAddresses[0].ipAddress}' -o json)
+  clientPrivateIP=$(echo $clientConfig | jq .privateip)
+  clientPrivateIP=$(sed -e "s/^'//" -e "s/'$//" <<<"$clientPrivateIP")
+  clientPrivateIP=$(sed -e 's/^"//' -e 's/"$//' <<<"$clientPrivateIP")
+  clientName=$(echo $clientConfig | jq .name)
+  clientName=$(sed -e "s/^'//" -e "s/'$//" <<<"$clientName")
+  clientName=$(sed -e 's/^"//' -e 's/"$//' <<<"$clientName")
+  clientPublicIP=$(echo $clientConfig | jq .publicip)
+  clientPublicIP=$(sed -e "s/^'//" -e "s/'$//" <<<"$clientPublicIP")
+  clientPublicIP=$(sed -e 's/^"//' -e 's/"$//' <<<"$clientPublicIP")
+
+  # Run ssh-keygen on client VM
+  ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@$clientPublicIP 'ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -P "" <<<y 2>&1 >/dev/null '
+  # Scp the client id_rsa.pub to local directory
+  scp tidb@$clientPublicIP:~/.ssh/id_rsa.pub ./client_rsa.pub
+
+  # Create servers with both local ssh key and client VM ssh key
+  for (( i=1; i<=noOfServers; i++ ))
+  do
+    az vm create --name mongodb"$namePrefix"-"$i" --resource-group DepFast3 --subscription 'Last Chance' --zone 1 --image debian --os-disk-size-gb 64 --storage-sku Premium_LRS --data-disk-sizes-gb 64 --size Standard_D4s_v3 --admin-username tidb --ssh-key-values ~/.ssh/id_rsa.pub ./client_rsa.pub --accelerated-networking true
+  done
+}
+
+function write_config {
+	rm -f config.json
+	az vm list-ip-addresses --ids $(az vm list --query "[].id" --resource-group DepFast3 -o tsv | grep $serverRegex) --query '[].{name:virtualMachine.name, privateip:virtualMachine.network.privateIpAddresses[0], publicip:virtualMachine.network.publicIpAddresses[0].ipAddress}' -o json > config.json
+}
+
+# Set the IPs of the given VM
+function find_ip {
+  for (( i=0; i<$noOfServers; i++ ))
+  do  
+    servername=$(cat config.json  | jq .[$i].name)
+	  servername="${servername%\"}"
+	  servername="${servername#\"}"
+    serverip=$(cat config.json  | jq .[$i].publicip)
+	  serverip="${serverip%\"}"
+	  serverip="${serverip#\"}"
+    serverNameIPMap[$servername]=$serverip
+  done
+}
+
+# Setup the servers to install tools and DB
+function setup_servers {
+  for key in "${!serverNameIPMap[@]}";
+  do
+    #Run the commands
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'sudo apt install tmux wget git --assume-yes ; sudo apt-get install cgroup-tools --assume-yes ; sudo apt-get install xfsprogs libc6 python python3 numactl --assume-yes ; ulimit -n 21000 ; ulimit -n 192276'"
+    ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'sudo parted -s -a optimal /dev/sdc mklabel gpt -- mkpart primary ext4 1 -1 '"
+    ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'rm -r mongodb ; wget -q https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.0.tgz ; tar -xf mongodb-linux-x86_64-ubuntu1804-4.4.0.tgz ; mv mongodb-linux-x86_64-ubuntu1804-4.4.0 mongodb'"
+    scp deadloop tidb@"${serverNameIPMap[$key]}":~/
+	  #Sync clocks - https://www.cockroachlabs.com/docs/stable/deploy-cockroachdb-on-microsoft-azure-insecure.html
+	  # TODO: specially for azure
+  	ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'curl -O https://raw.githubusercontent.com/torvalds/linux/master/tools/hv/lsvmbus'"
+  	devID=$(ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "python lsvmbus -vv | grep -w \"Time Synchronization\" -A 3 | grep Device_ID | grep -o '{.*}' | tr -d "{}"")
+	  ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'echo "$devID" | sudo tee /sys/bus/vmbus/drivers/hv_util/unbind'"
+  	ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'sudo apt-get install ntp ntpstat --assume-yes ; sudo service ntp stop ; sudo ntpd -b time.google.com'"
+  	ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'echo -e \"server time1.google.com iburst\nserver time2.google.com iburst\nserver time3.google.com iburst\nserver time4.google.com iburst\" >> /etc/ntp.conf'"
+  	ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sh -c 'sudo service ntp start ; ntpstat ; true'"
+    scp disableTHP tidb@"${serverNameIPMap[$key]}":~/
+    ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo cp disableTHP /etc/systemd/system/disable-transparent-huge-pages.service"
+    ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo systemctl daemon-reload ; sudo systemctl start disable-transparent-huge-pages ; cat /sys/kernel/mm/transparent_hugepage/enabled ; sudo systemctl enable disable-transparent-huge-pages"
+    # TODO: azure specific settings
+    ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo sysctl -w net.ipv4.tcp_keepalive_time=120"
+    ssh -i ~/.ssh/id_rsa tidb@${serverNameIPMap[$key]} "sudo echo 'net.ipv4.tcp_keepalive_time = 120' | sudo tee /etc/sysctl.conf"
+  done
+}
+
+function setup_client {
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@$clientPublicIP << EOF_1
+	sudo apt install git default-jre --assume-yes
+	sudo apt install maven --assume-yes
+	curl -O --location https://github.com/brianfrankcooper/YCSB/releases/download/0.17.0/ycsb-0.17.0.tar.gz ; tar xfvz ycsb-0.17.0.tar.gz
+	(cd ~/ycsb-0.17.0/jdbc-binding/lib/; wget https://jdbc.postgresql.org/download/postgresql-42.2.10.jar)
+	curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+	sudo apt install jq --assume-yes
+	rm -r mongodb ; wget -q https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu1804-4.4.0.tgz ; tar -xf mongodb-linux-x86_64-ubuntu1804-4.4.0.tgz ; mv mongodb-linux-x86_64-ubuntu1804-4.4.0 mongodb
+#	git clone https://github.com/WolfDOS/gray-testing.git
+EOF_1
+  # SCP the experiment files to the client. This should run from the script/cockroachdb path
+	#scp -r ./* tidb@$clientPublicIP:~/ycsb-0.17.0/
+}
+
+function setup_client_az {
+	# Create a service principal for azure login from the client VM
+	rm -f serviceprincipal.json
+	az ad sp create-for-rbac --name $namePrefix > serviceprincipal.json
+	appID=$(cat serviceprincipal.json | jq .appId)
+  appID=$(sed -e "s/^'//" -e "s/'$//" <<<"$appID")
+	password=$(cat serviceprincipal.json | jq .password)
+  password=$(sed -e "s/^'//" -e "s/'$//" <<<"$password")
+	tenantID=$(cat serviceprincipal.json | jq .tenant)
+  tenantID=$(sed -e "s/^'//" -e "s/'$//" <<<"$tenantID")
+	sleep 30
+	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@$clientPublicIP "az login --service-principal --username $appID --password $password --tenant $tenantID"
+}
+
+function deallocate_vms {
+	ns=$(az vm list --query "[].id" --resource-group DepFast3 -o tsv | grep $serverRegex | wc -l)
+	if [[ $ns -le 5 ]]
+	then
+		az vm deallocate --ids $(
+			az vm list --query "[].id" --resource-group DepFast3 -o tsv | grep $serverRegex
+		)
+	else
+		echo "Server regex malformed, performing linear stop"
+		# Switching back to linear stop
+		for key in "${!serverNameIPMap[@]}";
+		do
+			az vm deallocate --name "$key" --resource-group "$resource"
+		done
+	fi
+	az vm deallocate --name "$clientName" --resource-group "$resource"
+}
+
+#function run_ssd_experiment {
+#	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@$clientPublicIP "(cd ~/ycsb-0.17.0/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure noslowfollower disk swapoff 3 $serverRegex $threadsycsb)"
+#}
+#
+#function run_memory_experiment {
+#	ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa tidb@$clientPublicIP "(cd ~/ycsb-0.17.0/ ; ./start_experiment.sh $iterations workloads/$workload $ycsbruntime 1 azure follower memory swapoff 3 $serverRegex $threadsycsb)"
+#}
+
+function main {
+#  setup_localvm    # for debug only
+
+  az_vm_create
+
+  write_config
+
+  find_ip
+
+  setup_servers
+
+  setup_client
+
+  # setup_client_az
+
+#  if [ "$filesystem" == "disk" ]; then
+#	run_ssd_experiment
+#  elif [ "$filesystem" == "memory" ]; then
+#	run_memory_experiment
+#  else
+#		echo "This option in filesystem is not supported.Exiting."
+#		exit 1
+#  fi
+
+#  deallocate_vms
+}
+
+main
---
+++ scripts/mongodb/start_experiment_hdd.sh
@@ -0,0 +1,267 @@
+#!/bin/bash
+
+#date=$(date +"%Y%m%d%s")
+#exec > "$date"_experiment.log
+#exec 2>&1
+
+set -ex
+
+# Server specific configs
+##########################
+s1="10.0.0.13"
+s2="10.0.0.14"
+s3="10.0.0.15"
+
+s1name="mongodb0-1"
+s2name="mongodb0-2"
+s3name="mongodb0-3"
+serverZone="us-central1-a"
+###########################
+
+if [ "$#" -ne 9 ]; then
+    echo "Wrong number of parameters"
+    echo "1st arg - number of iterations"
+    echo "2nd arg - workload path"
+    echo "3rd arg - seconds to run ycsb run"
+    echo "4th arg - experiment to run(1, 2, 3 only for hdd, 4 only for hdd, 5, 6 only for swapon+mem)"
+    echo "5th arg - host type(gcp/azure)"
+    echo "6th arg - type of experiment(follower/leader/noslow)"
+    echo "7th arg - turn on swap (swapon/swapoff) [swapon only for exp6+mem] "
+    echo "8th arg - in disk or in memory (hdd/mem)"
+	echo "9th arg - threads for ycsb run(for saturation exp)"
+    exit 1
+fi
+
+iterations=$1
+workload=$2
+ycsbruntime=$3
+expno=$4
+host=$5
+exptype=$6
+swapness=$7
+ondisk=$8
+ycsbthreads=$9
+
+# test_start is executed at the beginning
+function test_start {
+  name=$1
+  echo "Running $exptype experiment $expno $swapness $ondisk for $name"
+  dirname="$name"_"$exptype"_"$swapness"_"$ondisk"_"$ycsbthreads"_results
+  mkdir -p $dirname
+}
+
+# data_cleanup is called just after servers start
+function data_cleanup {
+  if [ "$ondisk" == "mem" ] ; then
+    ssh -i ~/.ssh/id_rsa "$s1" "sh -c 'sudo rm -rf /ramdisk/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s2" "sh -c 'sudo rm -rf /ramdisk/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s3" "sh -c 'sudo rm -rf /ramdisk/mongodb-data'"
+  else
+    ssh -i ~/.ssh/id_rsa "$s1" "sh -c 'sudo rm -rf /data1/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s2" "sh -c 'sudo rm -rf /data1/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s3" "sh -c 'sudo rm -rf /data1/mongodb-data'"
+  fi
+}
+
+function start_servers {
+  if [ "$host" == "gcp" ]; then
+    gcloud compute instances start "$s1name" "$s2name" "$s3name" --zone="$serverZone"
+  elif [ "$host" == "azure" ]; then
+    az vm start --resource-group DepFast3 --name "$s1name"
+    az vm start --resource-group DepFast3 --name "$s2name"
+    az vm start --resource-group DepFast3 --name "$s3name"
+  else
+    echo "Not implemented error"
+    exit 1
+  fi
+  sleep 30
+}
+
+# init is called to initialise the db servers
+function init {
+  ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo umount /dev/sdc1 ; sudo mkdir -p /data1 ; sudo mkfs.ext4 /dev/sdc1 -F ; sudo mount -t ext4 /dev/sdc1 /data1 -o defaults,nodelalloc,noatime ; sudo chmod o+w /data1/ ; mkdir /data1/mongodb-data ; sudo chmod o+w /data1/mongodb-data'"
+  ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo umount /dev/sdc1 ; sudo mkdir -p /data1 ; sudo mkfs.ext4 /dev/sdc1 -F ; sudo mount -t ext4 /dev/sdc1 /data1 -o defaults,nodelalloc,noatime ; sudo chmod o+w /data1/ ; mkdir /data1/mongodb-data ; sudo chmod o+w /data1/mongodb-data'"
+  ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo umount /dev/sdc1 ; sudo mkdir -p /data1 ; sudo mkfs.ext4 /dev/sdc1 -F ; sudo mount -t ext4 /dev/sdc1 /data1 -o defaults,nodelalloc,noatime ; sudo chmod o+w /data1/ ; mkdir /data1/mongodb-data ; sudo chmod o+w /data1/mongodb-data'"
+
+  if [ "$swapness" == "swapoff" ] ; then
+    ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo sysctl vm.swappiness=0 ; sudo swapoff -a && swapon -a'"
+    ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo sysctl vm.swappiness=0 ; sudo swapoff -a && swapon -a'"
+    ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo sysctl vm.swappiness=0 ; sudo swapoff -a && swapon -a'"
+  else
+    ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo dd if=/dev/zero of=/data1/swapfile bs=1024 count=25165824 ; sudo chmod 600 /data1/swapfile ; sudo mkswap /data1/swapfile'"  # 24GB
+    ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo sysctl vm.swappiness=60 ; sudo swapoff -a && swapon -a ; sudo swapon /data1/swapfile'"
+    ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo dd if=/dev/zero of=/data1/swapfile bs=1024 count=25165824 ; sudo chmod 600 /data1/swapfile ; sudo mkswap /data1/swapfile'"  # 24GB
+    ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo sysctl vm.swappiness=60 ; sudo swapoff -a && swapon -a ; sudo swapon /data1/swapfile'"
+    ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo dd if=/dev/zero of=/data1/swapfile bs=1024 count=25165824 ; sudo chmod 600 /data1/swapfile ; sudo mkswap /data1/swapfile'"  # 24GB
+    ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo sysctl vm.swappiness=60 ; sudo swapoff -a && swapon -a ; sudo swapon /data1/swapfile'"
+  fi
+
+  if [ "$ondisk" == "mem" ] ; then
+    ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'sudo mkdir -p /ramdisk ; sudo mount -t tmpfs -o rw,size=8G tmpfs /ramdisk/ ; sudo chmod o+w /ramdisk/ ; mkdir /ramdisk/mongodb-data ; sudo chmod o+w /ramdisk/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'sudo mkdir -p /ramdisk ; sudo mount -t tmpfs -o rw,size=8G tmpfs /ramdisk/ ; sudo chmod o+w /ramdisk/ ; mkdir /ramdisk/mongodb-data ; sudo chmod o+w /ramdisk/mongodb-data'"
+    ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'sudo mkdir -p /ramdisk ; sudo mount -t tmpfs -o rw,size=8G tmpfs /ramdisk/ ; sudo chmod o+w /ramdisk/ ; mkdir /ramdisk/mongodb-data ; sudo chmod o+w /ramdisk/mongodb-data'"
+  fi
+}
+
+# start_db starts the database instances on each of the server
+function start_db {
+  if [ "$ondisk" == "mem" ] ; then
+    ssh  -i ~/.ssh/id_rsa "$s1" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s1name" --fork --logpath /tmp/mongod.log --dbpath /ramdisk/mongodb-data'"
+    ssh  -i ~/.ssh/id_rsa "$s2" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s2name" --fork --logpath /tmp/mongod.log --dbpath /ramdisk/mongodb-data'"
+    ssh  -i ~/.ssh/id_rsa "$s3" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s3name" --fork --logpath /tmp/mongod.log --dbpath /ramdisk/mongodb-data'"
+  else
+    ssh  -i ~/.ssh/id_rsa "$s1" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s1name" --fork --logpath /tmp/mongod.log --dbpath /data1/mongodb-data'"
+    ssh  -i ~/.ssh/id_rsa "$s2" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s2name" --fork --logpath /tmp/mongod.log --dbpath /data1/mongodb-data'"
+    ssh  -i ~/.ssh/id_rsa "$s3" "sh -c 'numactl --interleave=all taskset -ac 0 /home/tidb/mongodb/bin/mongod --replSet rs0 --bind_ip localhost,"$s3name" --fork --logpath /tmp/mongod.log --dbpath /data1/mongodb-data'"
+  fi
+  sleep 30
+}
+
+# db_init initialises the database
+function db_init {
+  /home/tidb/mongodb/bin/mongo --host "$s1name" < init_script.js
+
+  # Wait for startup
+  sleep 60
+
+  /home/tidb/mongodb/bin/mongo --host "$s1name" < fetchprimary.js  | tail -n +5 | head -n -1  > result.json
+  cat result.json
+
+  primaryip=$(python parse.py | grep primary | cut -d" " -f2-)
+  secondaryip=$(python parse.py | grep secondary | cut -d" " -f2-)
+
+  primarypid=$(ssh -i ~/.ssh/id_rsa "$primaryip" "sh -c 'pgrep mongo'")
+  echo $primarypid
+
+  secondarypid=$(ssh -i ~/.ssh/id_rsa "$secondaryip" "sh -c 'pgrep mongo'")
+  echo $secondarypid
+
+  if [ "$exptype" == "follower" ]; then
+    slowdownpid=$secondarypid
+    slowdownip=$secondaryip  
+  elif [ "$exptype" == "leader" ]; then
+    slowdownpid=$primarypid
+    slowdownip=$primaryip
+  else
+    # Nothing to do
+    echo ""
+  fi
+
+  # Disable chaining allowed
+  /home/tidb/mongodb/bin/mongo --host $primaryip --eval "cfg = rs.config(); cfg.settings.chainingAllowed = false; rs.reconfig(cfg);"
+  #/home/tidb/mongodb/bin/mongo --host $s1 --eval "db.adminCommand( { replSetSyncFrom: '$primaryip' })"
+  /home/tidb/mongodb/bin/mongo --host mongodb0-2 --eval "db.adminCommand( { replSetSyncFrom: 'mongodb0-1:27017' })"
+  /home/tidb/mongodb/bin/mongo --host mongodb0-3 --eval "db.adminCommand( { replSetSyncFrom: 'mongodb0-1:27017' })"
+
+  # Set WriteConcern==majority    in order to make it consistent between all DBs
+  /home/tidb/mongodb/bin/mongo --host $primaryip --eval "cfg = rs.config(); cfg.settings.getLastErrorDefaults = { j:true, w:'majority', wtimeout:10000 }; rs.reconfig(cfg);"
+}
+
+# ycsb_load is used to run the ycsb load and wait until it completes.
+function ycsb_load {
+  cd /home/tidb/ycsb-0.17.0/bin
+  /home/tidb/ycsb-0.17.0/bin/ycsb load mongodb -s -P /home/tidb/gray-testing/scripts/mongodb/$workload  -threads 32 -p mongodb.url=mongodb://$primaryip:27017/ycsb?w=majority&readConcernLevel=majority ; wait $!
+  cd /home/tidb/gray-testing/scripts/mongodb
+}
+
+# ycsb run exectues the given workload and waits for it to complete
+function ycsb_run {
+  cd /home/tidb/ycsb-0.17.0/bin
+  /home/tidb/ycsb-0.17.0/bin/ycsb run mongodb -s -P /home/tidb/gray-testing/scripts/mongodb/$workload -threads $ycsbthreads  -p maxexecutiontime=$ycsbruntime -p mongodb.url="mongodb://$primaryip:27017/ycsb?w=majority&readConcernLevel=majority" > /home/tidb/gray-testing/scripts/mongodb/"$dirname"/exp"$expno"_trial_"$i".txt ; wait $!
+  #  -threads 32  for saturation
+  cd /home/tidb/gray-testing/scripts/mongodb
+}
+
+# cleanup is called at the end of the given trial of an experiment
+function mongo_cleanup {
+  /home/tidb/mongodb/bin/mongo --host "$primaryip" < cleanup_script.js
+  /home/tidb/mongodb/bin/mongo --host "$primaryip" --eval "db.getCollectionNames().forEach(function(n){db[n].remove()});"
+  rm result.json
+  sleep 5
+}
+
+function node_cleanup {
+#  ssh -i ~/.ssh/id_rsa "$s1" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
+#  ssh -i ~/.ssh/id_rsa "$s2" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
+#  ssh -i ~/.ssh/id_rsa "$s3" "sudo sh -c 'rm -rf /data/db ; sudo umount /dev/sdb ; sudo rm -rf /data/ ; sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db ; pkill mongod ; true'"
+#  # Remove the tc rule for exp 5
+#  if [ "$expno" == 5 -a "$exptype" != "noslow" ]; then
+#    ssh -i ~/.ssh/id_rsa "$slowdownip" "sudo sh -c 'sudo /sbin/tc qdisc del dev eth0 root ; true'"
+#  fi
+  ssh -i ~/.ssh/id_rsa tidb@"$s1" "sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db memory:db ; true"
+  ssh -i ~/.ssh/id_rsa tidb@"$s1" "sudo /sbin/tc qdisc del dev eth0 root ; true"
+  sleep 5
+  ssh -i ~/.ssh/id_rsa tidb@"$s2" "sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db memory:db ; true"
+  ssh -i ~/.ssh/id_rsa tidb@"$s2" "sudo /sbin/tc qdisc del dev eth0 root ; true"
+  sleep 5
+  ssh -i ~/.ssh/id_rsa tidb@"$s3" "sudo cgdelete cpu:db cpu:cpulow cpu:cpuhigh blkio:db memory:db ; true"
+  ssh -i ~/.ssh/id_rsa tidb@"$s3" "sudo /sbin/tc qdisc del dev eth0 root ; true"
+  sleep 5
+}
+
+# stop_servers turns off the VM instances
+function stop_servers {
+  if [ "$host" == "gcp" ]; then
+    gcloud compute instances stop "$s1name" "$s2name" "$s3name" --zone="$serverZone"
+  elif [ "$host" == "azure" ]; then
+    az vm deallocate --resource-group DepFast3 --name "$s1name"
+    az vm deallocate --resource-group DepFast3 --name "$s2name"
+    az vm deallocate --resource-group DepFast3 --name "$s3name"
+  else
+    echo "Not implemented error"
+    exit 1
+  fi
+}
+
+# run_experiment executes the given experiment
+function run_experiment {
+  ./experiment$expno.sh "$slowdownip" "$slowdownpid"
+}
+
+# test_run is the main driver function
+function test_run {
+  for (( i=1; i<=$iterations; i++ ))
+  do
+    echo "Running experiment $expno - Trial $i"
+    # 1. start servers
+    start_servers
+
+    # 2. Cleanup first
+    #data_cleanup  
+    node_cleanup
+
+    # 3. Create data directories
+    init
+
+    # 4. SSH to all the machines and start db
+    start_db
+
+    # 5. Init
+    db_init
+
+    # 6. ycsb load
+    ycsb_load
+
+    # 7. Run experiment if this is not a no slow
+    if [ "$exptype" != "noslow" ]; then
+      run_experiment
+    fi
+
+    # 8. ycsb run
+    ycsb_run
+
+    # 9. cleanup
+    mongo_cleanup
+    node_cleanup
+    #data_cleanup
+
+    # 10. Power off all the VMs
+    stop_servers
+  done
+}
+
+test_start mongodb
+test_run
+
+# Make sure either shutdown is executed after you run this script or uncomment the last line
+# sudo shutdown -h now
