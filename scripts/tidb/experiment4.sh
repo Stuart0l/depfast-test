bc90daaf817dcaf37fc2e79be4e6b30b85780547
pentium3 <celerond@msn.com>
2020-09-06 22:33:08 -0500

    ..

--- scripts/tidb/experiment4.sh
+++ scripts/tidb/experiment4.sh
@@ -1,7 +1,11 @@
- #!/bin/bash
+#!/bin/bash
 
- set -ex
+set -ex
 
- ip=$1
+ip=$1
 
- ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'sudo nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=200000000 > /dev/null 2>&1 &'"
+ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'sudo nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=200000000 > /dev/null 2>&1 &'"
+
+
+#200GB
+
--- scripts/tidb/saturate_hdd.sh
+++ scripts/tidb/saturate_hdd.sh
@@ -1,13 +1,13 @@
-#./start_experiment_hdd.sh 5 ./workloads 300 1 azure leaderhigh swapoff hdd 16
-#./start_experiment_hdd.sh 5 ./workloads 300 2 azure leaderhigh swapoff hdd 16
-./start_experiment_hdd.sh 5 ./workloads 300 3 azure leaderhigh swapoff hdd 16
-./start_experiment_hdd.sh 5 ./workloads 300 4 azure leaderhigh swapoff hdd 16
-#./start_experiment_hdd.sh 5 ./workloads 300 5 azure leaderhigh swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 1 azure leaderhigh swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 2 azure leaderhigh swapoff hdd 16
+./start_experiment_hdd.sh 5 ./workloada 300 3 azure leaderhigh swapoff hdd 16
+./start_experiment_hdd.sh 5 ./workloada 300 4 azure leaderhigh swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 5 azure leaderhigh swapoff hdd 16
 
-#./start_experiment_hdd.sh 5 ./workloads 300 1 azure leaderlow swapoff hdd 16
-#./start_experiment_hdd.sh 5 ./workloads 300 2 azure leaderlow swapoff hdd 16
-./start_experiment_hdd.sh 5 ./workloads 300 3 azure leaderlow swapoff hdd 16
-./start_experiment_hdd.sh 5 ./workloads 300 4 azure leaderlow swapoff hdd 16
-#./start_experiment_hdd.sh 5 ./workloads 300 5 azure leaderlow swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 1 azure leaderlow swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 2 azure leaderlow swapoff hdd 16
+./start_experiment_hdd.sh 5 ./workloada 300 3 azure leaderlow swapoff hdd 16
+./start_experiment_hdd.sh 5 ./workloada 300 4 azure leaderlow swapoff hdd 16
+#./start_experiment_hdd.sh 5 ./workloada 300 5 azure leaderlow swapoff hdd 16
 
-./start_experiment_hdd.sh 5 ./workloads 300 1 azure noslow1 swapoff hdd 16
+./start_experiment_hdd.sh 5 ./workloada 300 1 azure noslow1 swapoff hdd 16
