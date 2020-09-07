d15b1f0c783e0d6ce9616e7364520052514c0454
pentium3 <celerond@msn.com>
2020-09-06 23:36:23 -0500

    exp4 60GB

--- scripts/tidb/experiment4.sh
+++ scripts/tidb/experiment4.sh
@@ -4,8 +4,8 @@ set -ex
 
 ip=$1
 
-ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'sudo nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=200000000 > /dev/null 2>&1 &'"
+ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'sudo nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=60000000 > /dev/null 2>&1 &'"
 
 
-#200GB
+#60GB
 
