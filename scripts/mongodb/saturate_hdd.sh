ea5ec83a7f308407667a78fcb1972d2cf4d384be
Your Name <you@example.com>
2020-09-10 20:09:19 +0000

    xfs

--- scripts/mongodb/saturate_hdd.sh
+++ scripts/mongodb/saturate_hdd.sh
@@ -1,16 +1,15 @@
-#./start_experiment_hdd.sh 5 ./workloada 300 1 azure leader swapoff hdd 32
-#./start_experiment_hdd.sh 5 ./workloada 300 2 azure leader swapoff hdd 32
-./start_experiment_hdd.sh 5 ./workloada 300 3 azure leader swapoff hdd 32
-./start_experiment_hdd.sh 5 ./workloada 300 4 azure leader swapoff hdd 32
-#./start_experiment_hdd.sh 5 ./workloada 300 5 azure leader swapoff hdd 32
+./start_experiment_hdd.sh 5 ./workloads 300 1 azure leader swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 2 azure leader swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 3 azure leader swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 4 azure leader swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 5 azure leader swapoff hdd 320
 
-./start_experiment_hdd.sh 5 ./workloada 300 1 azure noslow swapoff hdd 32
+./start_experiment_hdd.sh 5 ./workloads 300 1 azure noslow swapoff hdd 320
 
-#./start_experiment_hdd.sh 5 ./workloada 300 1 azure follower swapoff hdd 32
-#./start_experiment_hdd.sh 5 ./workloada 300 2 azure follower swapoff hdd 32
-./start_experiment_hdd.sh 5 ./workloada 300 3 azure follower swapoff hdd 32
-./start_experiment_hdd.sh 5 ./workloada 300 4 azure follower swapoff hdd 32
-#./start_experiment_hdd.sh 5 ./workloada 300 5 azure follower swapoff hdd 32
-
+./start_experiment_hdd.sh 5 ./workloads 300 1 azure follower swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 2 azure follower swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 3 azure follower swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 4 azure follower swapoff hdd 320
+./start_experiment_hdd.sh 5 ./workloads 300 5 azure follower swapoff hdd 320
 
 
