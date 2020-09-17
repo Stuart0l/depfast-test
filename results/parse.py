a151b0e3082a550b2e3a0a495420b58b315bf18d
pentium3 <celerond@msn.com>
2020-09-17 16:10:27 -0500

    tidb exp6 + txp4

--- results/parse.py
+++ results/parse.py
@@ -23,6 +23,7 @@ def parseycsb1(fpath):
     return(res)
 
 def parseycsb2(fpath):
+    # print(fpath)
     ff=open(fpath,'r').read().split('\n')
     line=ff[-2].split(', ')
     res={}
@@ -41,6 +42,7 @@ def parseycsb2(fpath):
     return(res)
 
 def calcforexp(expname, fpath, dbname):
+    # print(expname, fpath, dbname)
     if('noslow' in expname):
         expname=''
     flist=os.listdir(fpath)
@@ -138,6 +140,7 @@ tidb_explist=[
     ['exp3','./1client_ssd/tidb/tidb_noslow1_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_leaderhigh_swapoff_hdd_1_results'],
     ['exp4','./1client_ssd/tidb/tidb_noslow1_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_leaderhigh_swapoff_hdd_1_results'],
     ['exp5','./1client_ssd/tidb/tidb_noslow1_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_leaderhigh_swapoff_hdd_1_results'],
+    ['exp6','./1client_ssd/tidb/tidb_noslow1_swapon_hdd_1_results','./1client_ssd/tidb/tidb_leaderhigh_swapon_hdd_1_results'],
     # leaderlow slowness ssd
     ['---'],
     ['exp1','./1client_ssd/tidb/tidb_noslow1_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_leaderlow_swapoff_hdd_1_results'],
@@ -152,6 +155,7 @@ tidb_explist=[
     ['exp3','./1client_ssd/tidb/tidb_noslow2_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_follower_swapoff_hdd_1_results'],
     ['exp4','./1client_ssd/tidb/tidb_noslow2_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_follower_swapoff_hdd_1_results'],
     ['exp5','./1client_ssd/tidb/tidb_noslow2_swapoff_hdd_1_results','./1client_ssd/tidb/tidb_follower_swapoff_hdd_1_results'],
+    ['exp6','./1client_ssd/tidb/tidb_noslow2_swapon_hdd_1_results','./1client_ssd/tidb/tidb_follower_swapon_hdd_1_results'],
 ]
 
 tidb_s_explist=[
@@ -174,6 +178,7 @@ tidb_s_explist=[
     ['exp3','./saturate_ssd/tidb/tidb_noslow1_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_leaderhigh_swapoff_hdd_512_results'],
     ['exp4','./saturate_ssd/tidb/tidb_noslow1_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_leaderhigh_swapoff_hdd_512_results'],
     ['exp5','./saturate_ssd/tidb/tidb_noslow1_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_leaderhigh_swapoff_hdd_512_results'],
+    ['exp6','./saturate_ssd/tidb/tidb_noslow1_swapon_hdd_512_results','./saturate_ssd/tidb/tidb_leaderhigh_swapon_hdd_512_results'],
     # leaderlow slowness ssd
     ['---'],
     ['exp1','./saturate_ssd/tidb/tidb_noslow1_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_leaderlow_swapoff_hdd_512_results'],
@@ -188,6 +193,7 @@ tidb_s_explist=[
     ['exp3','./saturate_ssd/tidb/tidb_noslow2_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_follower_swapoff_hdd_512_results'],
     ['exp4','./saturate_ssd/tidb/tidb_noslow2_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_follower_swapoff_hdd_512_results'],
     ['exp5','./saturate_ssd/tidb/tidb_noslow2_swapoff_hdd_512_results','./saturate_ssd/tidb/tidb_follower_swapoff_hdd_512_results'],
+    ['exp6','./saturate_ssd/tidb/tidb_noslow2_swapon_hdd_512_results','./saturate_ssd/tidb/tidb_follower_swapon_hdd_512_results'],
 ]
 
 
@@ -211,6 +217,7 @@ mongodb_explist=[
     ['exp3','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_leader_swapoff_hdd_1_results'],
     ['exp4','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_leader_swapoff_hdd_1_results'],
     ['exp5','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_leader_swapoff_hdd_1_results'],
+    ['exp6','./1client_ssd/mongodb/mongodb_noslow_swapon_hdd_1_results','./1client_ssd/mongodb/mongodb_leader_swapon_hdd_1_results'],
     # follower slowness hdd
     ['---'],
     ['exp1','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_follower_swapoff_hdd_1_results'],
@@ -218,6 +225,7 @@ mongodb_explist=[
     ['exp3','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_follower_swapoff_hdd_1_results'],
     ['exp4','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_follower_swapoff_hdd_1_results'],
     ['exp5','./1client_ssd/mongodb/mongodb_noslow_swapoff_hdd_1_results','./1client_ssd/mongodb/mongodb_follower_swapoff_hdd_1_results'],
+    ['exp6','./1client_ssd/mongodb/mongodb_noslow_swapon_hdd_1_results','./1client_ssd/mongodb/mongodb_follower_swapon_hdd_1_results'],
 ]
 
 mongodb_s_explist=[
@@ -240,6 +248,7 @@ mongodb_s_explist=[
     ['exp3','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_leader_swapoff_hdd_320_results'],
     ['exp4','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_leader_swapoff_hdd_320_results'],
     ['exp5','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_leader_swapoff_hdd_320_results'],
+    ['exp6','./saturate_ssd/mongodb/mongodb_noslow_swapon_hdd_320_results','./saturate_ssd/mongodb/mongodb_leader_swapon_hdd_320_results'],
     # follower slowness hdd
     ['---'],
     ['exp1','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_follower_swapoff_hdd_320_results'],
@@ -247,6 +256,7 @@ mongodb_s_explist=[
     ['exp3','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_follower_swapoff_hdd_320_results'],
     ['exp4','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_follower_swapoff_hdd_320_results'],
     ['exp5','./saturate_ssd/mongodb/mongodb_noslow_swapoff_hdd_320_results','./saturate_ssd/mongodb/mongodb_follower_swapoff_hdd_320_results'],
+    ['exp6','./saturate_ssd/mongodb/mongodb_noslow_swapon_hdd_320_results','./saturate_ssd/mongodb/mongodb_follower_swapon_hdd_320_results'],
 ]
 
 rethinkdb_explist=[
@@ -874,17 +884,17 @@ def draw(metric, _list, _lim, _legend=False):
         explist['noslow']={'ops': 0, 'avg': 0, '99': 0, '999': 0, 'max': 0}
         #print(explist)
         # TODO - Remove this line once all DB have exp 6 results
-        if x[1] not in ["rethinkdb", "cockroachdb"]:
+        if (x[1] =="mongodb"):
             explist['exp6']={'ops': 0, 'avg': 0, '99': 0, '999': 0, 'max': 0}
         
         for i, _e in enumerate(exp_label):
             dat=_e[1]
             normval=explist[dat][metric]/100+1
+            print(i, dat, explist[dat][metric], normval, x[1])
             barlabel=str(round(normval))
             if(round(normval)>1000):
                 barlabel=str(round(normval/1000,1))+'k'
             # barlabel=str(max(round(explist[dat][metric]/100), _lim[2]))+'x'
-            print(i, dat, explist[dat][metric], normval)
             if(x[1]=='rethinkdb' and (dat=='exp1' or dat=='exp2')):
                 plt.text(idx_tick_label[_l]+bar_width*i, 0.05, 'Crash', ha='center', fontsize=28, fontweight='bold', rotation=90)
             else:
@@ -910,50 +920,50 @@ def draw(metric, _list, _lim, _legend=False):
 
 
 drawlist_L1=[
-          [mongodb_explist, 'mongodb', '/1client_ssd/mongodb/mongodb_leader_swapoff_hdd', 'MongoDB'],
+          [mongodb_explist, 'mongodb', '/1client_ssd/mongodb/mongodb_leader_swap', 'MongoDB'],
           [rethinkdb_explist, 'rethinkdb', '/1client_ssd/rethinkdb/rethinkdb_leader_disk_swap', 'RethinkDB'],
-          [tidb_explist, 'tidb', '/1client_ssd/tidb/tidb_leaderhigh_swapoff_hdd', 'TiDB'],
+          [tidb_explist, 'tidb', '/1client_ssd/tidb/tidb_leaderhigh_swap', 'TiDB'],
           [cockroachdb_explist, 'cockroachdb', '/1client_ssd/cockroachdb/cockroachdb_maxthroughput_disk_swap', 'CRDB'],
          ]
 
 drawlist_F1=[
-          [mongodb_explist, 'mongodb', '/1client_ssd/mongodb/mongodb_follower_swapoff_hdd', 'MongoDB'],
+          [mongodb_explist, 'mongodb', '/1client_ssd/mongodb/mongodb_follower_swap', 'MongoDB'],
           [rethinkdb_explist, 'rethinkdb', '/1client_ssd/rethinkdb/rethinkdb_follower_disk_swap', 'RethinkDB'],
-          [tidb_explist, 'tidb', '/1client_ssd/tidb/tidb_follower_swapoff_hdd', 'TiDB'],
+          [tidb_explist, 'tidb', '/1client_ssd/tidb/tidb_follower_swap', 'TiDB'],
           [cockroachdb_explist, 'cockroachdb', '/1client_ssd/cockroachdb/cockroachdb_follower_disk_swap', 'CRDB'],
          ]
 
 drawlist_LS=[
-          [mongodb_s_explist, 'mongodb', '/saturate_ssd/mongodb/mongodb_leader_swapoff_hdd', 'MongoDB'],
+          [mongodb_s_explist, 'mongodb', '/saturate_ssd/mongodb/mongodb_leader_swap', 'MongoDB'],
           [rethinkdb_s_explist, 'rethinkdb', '/saturate_ssd/rethinkdb/rethinkdb_leader_disk_swap', 'RethinkDB'],
-          [tidb_s_explist, 'tidb', '/saturate_ssd/tidb/tidb_leaderhigh_swapoff_hdd', 'TiDB'],
+          [tidb_s_explist, 'tidb', '/saturate_ssd/tidb/tidb_leaderhigh_swap', 'TiDB'],
           [cockroachdb_s_explist, 'cockroachdb', '/saturate_ssd/cockroachdb/cockroachdb_maxthroughput_disk_swap', 'CRDB'],
          ]
 
 drawlist_FS=[
-          [mongodb_s_explist, 'mongodb', '/saturate_ssd/mongodb/mongodb_follower_swapoff_hdd', 'MongoDB'],
+          [mongodb_s_explist, 'mongodb', '/saturate_ssd/mongodb/mongodb_follower_swap', 'MongoDB'],
           [rethinkdb_s_explist, 'rethinkdb', '/saturate_ssd/rethinkdb/rethinkdb_follower_disk_swap', 'RethinkDB'],
-          [tidb_s_explist, 'tidb', '/saturate_ssd/tidb/tidb_follower_swapoff_hdd', 'TiDB'],
+          [tidb_s_explist, 'tidb', '/saturate_ssd/tidb/tidb_follower_swap', 'TiDB'],
           [cockroachdb_s_explist, 'cockroachdb', '/saturate_ssd/cockroachdb/cockroachdb_follower_disk_swap', 'CRDB'],
          ]
 
 
 
-getpercentage(mongodb_explist, 'mongodb')
-exportcsv(mongo_csv, 'mongodb')
-getpercentage(mongodb_s_explist, 'mongodb')
-exportcsv(mongo_s_csv, 'mongodb')
+# getpercentage(mongodb_explist, 'mongodb')
+# exportcsv(mongo_csv, 'mongodb')
+# getpercentage(mongodb_s_explist, 'mongodb')
+# exportcsv(mongo_s_csv, 'mongodb')
 
-getpercentage(tidb_explist, 'tidb')
-exportcsv(tidb_csv, 'tidb')
-getpercentage(tidb_s_explist, 'tidb')
-exportcsv(tidb_s_csv, 'tidb')
+# getpercentage(tidb_explist, 'tidb')
+# exportcsv(tidb_csv, 'tidb')
+# getpercentage(tidb_s_explist, 'tidb')
+# exportcsv(tidb_s_csv, 'tidb')
 
-getpercentage(cockroachdb_s_explist, 'cockroachdb')
-exportcsv(cockroachdb_s_csv, 'cockroachdb')
+# getpercentage(cockroachdb_s_explist, 'cockroachdb')
+# exportcsv(cockroachdb_s_csv, 'cockroachdb')
 
-getpercentage(cockroachdb_explist, 'cockroachdb')
-exportcsv(cockroachdb_csv, 'cockroachdb')
+# getpercentage(cockroachdb_explist, 'cockroachdb')
+# exportcsv(cockroachdb_csv, 'cockroachdb')
 
 sizex=24
 sizey=8
@@ -974,35 +984,35 @@ DY=0.91
 # plt.show()
 
 
+# plt.figure(figsize=(28,9*DX), dpi=sizei)
+# draw('ops',drawlist_L1, [0,1.15,1, 0], _legend=True)
+# plt.savefig('L1ops.pdf')
+
+# plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
+# draw('avg',drawlist_L1, [0,46,40, 0.5])
+# plt.savefig('L1avg.pdf')
+
+# plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
+# draw('99',drawlist_L1, [0,46,40, 0.5])
+# plt.savefig('L199.pdf')
+
+
+
+# plt.figure(figsize=(28,9*DX), dpi=sizei)
+# draw('ops',drawlist_F1, [0,1.15,1, 0], _legend=True)
+# plt.savefig('F1ops.pdf')
+
+# plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
+# draw('avg',drawlist_F1, [0,2.3,2, 0.02])
+# plt.savefig('F1avg.pdf')
+
+# plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
+# draw('99',drawlist_F1, [0,2.3,2, 0.02])
+# plt.savefig('F199.pdf')
+
+
+
 plt.figure(figsize=(28,9*DX), dpi=sizei)
-draw('ops',drawlist_L1, [0,1.15,1, 0], _legend=True)
-plt.savefig('L1ops.pdf')
-
-plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
-draw('avg',drawlist_L1, [0,46,40, 0.5])
-plt.savefig('L1avg.pdf')
-
-plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
-draw('99',drawlist_L1, [0,46,40, 0.5])
-plt.savefig('L199.pdf')
-
-
-
-plt.figure(figsize=(28,9*DX), dpi=sizei)
-draw('ops',drawlist_F1, [0,1.15,1, 0], _legend=True)
-plt.savefig('F1ops.pdf')
-
-plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
-draw('avg',drawlist_F1, [0,2.3,2, 0.02])
-plt.savefig('F1avg.pdf')
-
-plt.figure(figsize=(sizex,sizey*DY), dpi=sizei)
-draw('99',drawlist_F1, [0,2.3,2, 0.02])
-plt.savefig('F199.pdf')
-
-
-
-plt.figure(figsize=(sizex,sizey*DX), dpi=sizei)
 draw('ops',drawlist_LS, [0,1.15,1,0], _legend=True)
 plt.savefig('LSops.pdf')
 
