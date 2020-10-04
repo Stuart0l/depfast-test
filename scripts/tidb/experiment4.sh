c5308bcdd1bc3d52be0f7a9e6be5602408759abb
pentium3 <celerond@msn.com>
2020-10-03 22:15:30 -0500

    ...

--- scripts/tidb/experiment4.sh
+++ scripts/tidb/experiment4.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
  set -ex
 
@@ -8,14 +8,9 @@
 #
 # #ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'rm /data1/placeholder'"
 #
-# ssh -i ~/.ssh/id_rsa "$ip" "sh -c 'nohup taskset -ac 2 ./clear_dd_file.sh > /dev/null 2>&1 &'"
+ ssh -i ~/.ssh/id_rsa "$ip" "sh -c 'nohup taskset -ac 2 ./clear_dd_file.sh > /dev/null 2>&1 &'"
 #
 # ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=60000000 > /dev/null 2>&1 &'"
 #
 
 
-
-while(true)
-do
-        ssh -i ~/.ssh/id_rsa "$ip" "sudo sh -c 'nohup taskset -ac 1 dd if=/dev/zero of=/data1/tmp.txt bs=1000 count=1600000 conv=notrunc'"
-done
