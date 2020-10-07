879f79041a700d46ec35cb5a257af2dbbee90295
ritesh <ritesh_sinha@ymail.com>
2020-10-06 23:28:01 -0700

    Fine-tuned value for mem contention experiment of Rethink and CRDB
    Systematic way to find this -
    Step 1. Start with 1 MB cgroup limit first.
    Step 2. If the DB crashes or we see timeout in requests, increase the limit by 100% ie. 1MB -> 2 MB -> 4MB and so on until DB doesn’t crash.
    Step 3. Once the DB crash is no longer seen, let’s say at 4MB, then we find the mean value of the previous value where the crash was seen(2MB) and last value used(4MB), i.e. 3 MB.
          If we see crash again on 3 MB, we now find mean of 3 MB and 4 MB, which is 3.5 MB.
          If we don’t crash at 3 MB, then find mean of 2MB and 3MB which is 2.5 MB. Use this new value as the limit and keep repeating step #3 until the difference between the new value and previous value > 0.25 MB. (Step #3 is similar to binary search algo where we get a potential interval and keep converging).

--- scripts/cockroachdb/experiment6.sh
+++ scripts/cockroachdb/experiment6.sh
@@ -6,6 +6,6 @@ targetip=$1
 targetpid=$2
 
 ssh -i ~/.ssh/id_rsa "$targetip" "sudo sh -c 'sudo mkdir /sys/fs/cgroup/memory/db'"
-ssh -i ~/.ssh/id_rsa "$targetip" "sudo sh -c 'sudo echo 10485760 > /sys/fs/cgroup/memory/db/memory.limit_in_bytes'"   # 10MB
+ssh -i ~/.ssh/id_rsa "$targetip" "sudo sh -c 'sudo echo 2375000 > /sys/fs/cgroup/memory/db/memory.limit_in_bytes'"   # 2.375MB
 ssh -i ~/.ssh/id_rsa "$targetip" "sudo sh -c 'sudo echo $targetpid > /sys/fs/cgroup/memory/db/cgroup.procs'"
-sleep 60
+sleep 5
