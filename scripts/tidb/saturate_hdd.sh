a622cfbbe5f43db5dfb89ca54a4155e8b807c0c0
Your Name <you@example.com>
2021-01-15 02:44:45 +0000

    ..

--- scripts/tidb/saturate_hdd.sh
+++ scripts/tidb/saturate_hdd.sh
@@ -14,12 +14,15 @@
 
 ./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
 
-./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure follower swapoff hdd 512
-./start_experiment_hdd.sh 1 ./workloadsd 300 2 azure follower swapoff hdd 512
-./start_experiment_hdd.sh 1 ./workloadsd 300 3 azure follower swapoff hdd 512
-./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
-./start_experiment_hdd.sh 1 ./workloadsd 300 5 azure follower swapoff hdd 512
+mv tidblogs tidblogs_noslow2
 
+#./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure follower swapoff hdd 512
+#./start_experiment_hdd.sh 1 ./workloadsd 300 2 azure follower swapoff hdd 512
+#./start_experiment_hdd.sh 1 ./workloadsd 300 3 azure follower swapoff hdd 512
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+#./start_experiment_hdd.sh 1 ./workloadsd 300 5 azure follower swapoff hdd 512
+
+mv tidblogs tidblogs_slow4
 
 #./start_experiment_hdd.sh 5 ./workloadsd 300 6 azure leaderhigh swapon hdd 512
 #./start_experiment_hdd.sh 5 ./workloadsd 300 6 azure leaderlow swapon hdd 512
