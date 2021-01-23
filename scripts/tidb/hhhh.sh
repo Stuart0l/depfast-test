76c1948714686dcbbe1c1042ae3011d4eefb2a22
Your Name <you@example.com>
2021-01-23 22:09:36 +0000

    find root cause

---
+++ scripts/tidb/hhhh.sh
@@ -0,0 +1,26 @@
+
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_noslow21
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow41
+
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_noslow22
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow42
+
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_noslow23
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow43
+
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_noslow24
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow44
+
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_noslow25
+./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow45
+
--- scripts/tidb/saturate_hdd.sh
+++ scripts/tidb/saturate_hdd.sh
@@ -12,17 +12,15 @@
 #./start_experiment_hdd.sh 5 ./workloadsd 300 4 azure leaderlow swapoff hdd 512
 #./start_experiment_hdd.sh 5 ./workloadsd 300 5 azure leaderlow swapoff hdd 512
 
-./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+#./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
 
-mv tidblogs tidblogs_noslow2
 
 #./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure follower swapoff hdd 512
 #./start_experiment_hdd.sh 1 ./workloadsd 300 2 azure follower swapoff hdd 512
 #./start_experiment_hdd.sh 1 ./workloadsd 300 3 azure follower swapoff hdd 512
-./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
+#./start_experiment_hdd.sh 1 ./workloadsd 300 4 azure follower swapoff hdd 512
 #./start_experiment_hdd.sh 1 ./workloadsd 300 5 azure follower swapoff hdd 512
 
-mv tidblogs tidblogs_slow4
 
 #./start_experiment_hdd.sh 5 ./workloadsd 300 6 azure leaderhigh swapon hdd 512
 #./start_experiment_hdd.sh 5 ./workloadsd 300 6 azure leaderlow swapon hdd 512
@@ -31,6 +29,24 @@ mv tidblogs tidblogs_slow4
 #./start_experiment_hdd.sh 5 ./workloadsd 300 1 azure noslow2 swapon hdd 512
 
 
+
+
+sed -i 's#raftstore.store-pool-size: 1#raftstore.store-pool-size: 4#g' tidb_restrict_hdd.yaml 
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_no4
+mv tidb_follower_swapoff_hdd_512_results/exp5_trial_1.txt ./tidbycsb_no4
+./start_experiment_hdd.sh 1 ./workloadsd 300 5 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow4
+mv tidb_follower_swapoff_hdd_512_results/exp5_trial_1.txt ./tidbycsb_slow4
+
+
+sed -i 's#raftstore.store-pool-size: 4#raftstore.store-pool-size: 1#g' tidb_restrict_hdd.yaml
+./start_experiment_hdd.sh 1 ./workloadsd 300 1 azure noslow2 swapoff hdd 512
+mv tidblogs tidblogs_no1
+mv tidb_follower_swapoff_hdd_512_results/exp5_trial_1.txt ./tidbycsb_no1
+./start_experiment_hdd.sh 1 ./workloadsd 300 5 azure follower swapoff hdd 512
+mv tidblogs tidblogs_slow1
+mv tidb_follower_swapoff_hdd_512_results/exp5_trial_1.txt ./tidbycsb_slow1
 
 
 
